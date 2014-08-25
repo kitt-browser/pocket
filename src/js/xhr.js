9fe0c1a5abd33fdbf4e2c90dd60874ec56b1e58c
Pavel Zdeněk <pavel@salsitasoft.com>
2014-08-25 18:39:33 +0200

    Make XHR compatible with new Kitt implementation

--- src/js/xhr.js
+++ src/js/xhr.js
@@ -13,7 +13,12 @@ exports.post = function (url, data) {
   xhr.onreadystatechange = function () {
     console.log('ready state change, state:' + xhr.readyState + ' ' + xhr.status);
     if (xhr.readyState === 4 && xhr.status === 200) {
-      defer.resolve(JSON.parse(xhr.responseText));
+      if(xhr.responseType === 'json') {
+        defer.resolve(xhr.response);
+      } else {
+        // backward compatibility with previous versions of Kitt
+        defer.resolve(JSON.parse(xhr.responseText));
+      }
     } else if (this.readyState === 4 && this.status === 401) {
       console.log('HTTP 401 returned');
       defer.reject({code: 401});
